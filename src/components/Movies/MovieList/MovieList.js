cdfc7462aaf1ece15e600f402da6f2a7327486e5
Shedrach Okonofua <shedrachokonofua@gmail.com>
2019-04-23 13:23:39 -0400

    Movie List Component

---
+++ src/components/Movies/MovieList/MovieList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import MoviePoster from './MoviePoster/MoviePoster';
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const MovieList = ({ loading, movies }) => {
+  if(loading) return <p>Loading...</p>;
+
+  return (
+    <Wrapper>
+      { movies.map(movie => (
+        <Link to={`/movie/${movie.id}`}>
+          <MoviePoster src={movie.posterUrl} title={movie.title}/>
+        </Link>      
+      )) }
+    </Wrapper>
+  );
+}
+
+MovieList.propTypes = {
+  loading: PropTypes.bool,
+  movies: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    posterUrl: PropTypes.string.isRequired, 
+    title: PropTypes.string.isRequired
+  })
+};
+
+MovieList.defaultProps = {
+  loading: false,
+};
+
+export default MovieList;
---
+++ src/components/Movies/MovieList/MovieList.stories.js
@@ -0,0 +1,32 @@
+import { storiesOf } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
+import { action } from '@storybook/addon-actions';
+import MovieList from './MovieList';
+
+const mockMovies = [
+  {
+    "id": "19404",
+    "title": "Dilwale Dulhania Le Jayenge",
+    "posterUrl": "http://image.tmdb.org/t/p/w185/uC6TTUhPpQCmgldGyYveKRAu8JN.jpg",
+  },{
+    "id": "278", 
+    "title":"The Shawshank Redemption",
+    "posterUrl":"http://image.tmdb.org/t/p/w185/9O7gLzmreU0nGkIB6K3BsJbzvNv.jpg"
+  },{
+    "id": "238",
+    "title":"The Godfather",
+    "posterUrl":"http://image.tmdb.org/t/p/w185/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg",
+  },{
+    "id": "372058",
+    "title":"Your Name.",
+    "posterUrl":"http://image.tmdb.org/t/p/w185/xq1Ugd62d23K2knRUx6xxuALTZB.jpg"
+  }
+];
+
+storiesOf('MovieList', module)
+  .addDecorator(StoryRouter({
+    '/movie/:id': action('Redirect to movie details')
+  }))
+  .addDecorator(storyFn => <div style={{ padding: '20px', width: '25%' }}>{storyFn()}</div>)
+  .add('loading', () => <MovieList loading movies={[]}/>)
+  .add('default', () => <MovieList movies={mockMovies}/>);
