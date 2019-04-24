63d695dc2ba7478bf7808d72e9b70e4fc4c2639f
Shedrach Okonofua <shedrachokonofua@gmail.com>
2019-04-24 01:53:14 -0400

    MoviePoster story style decorator changes

--- src/components/Movies/MovieList/MoviePoster/MoviePoster.stories.js
+++ src/components/Movies/MovieList/MoviePoster/MoviePoster.stories.js
@@ -5,5 +5,5 @@ import MoviePoster from './MoviePoster';
 const posterSrc = 'http://image.tmdb.org/t/p/w185/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg';
 
 storiesOf('MoviePoster', module)
-  .addDecorator(storyFn => <div style={{ width: '185px' }}>{storyFn()}</div>)
+  .addDecorator(storyFn => <div style={{ width: '50%' }}>{storyFn()}</div>)
   .add('default', () => <MoviePoster title='Testing' src={posterSrc}/>)
